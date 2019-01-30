b9fd637d7a740d4d4f806a0b1113168383448c65
christopherforrest <christopherforrest92@gmail.com>
2019-01-30 15:47:38 +1100

    fix cors

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import { SecureRoute, Security, ImplicitCallback } from '@okta/okta-react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Home from './Home';
 
-// const config = {
-//   issuer: 'https://dev-493904.oktapreview.com/oauth2/default',
-//   redirect_uri: window.location.origin + '/implicit/callback',
-//   client_id: '0oai9gwg86y6bzzft0h7'
-// }
+const config = {
+  issuer: 'https://dev-493904.oktapreview.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '0oai9gwg86y6bzzft0h7'
+}
 
 
 class App extends Component {
@@ -24,7 +24,7 @@ class App extends Component {
     return (
       <div className="App">
         <Router>
-          <Security issuer={'https://dev-493904.oktapreview.com/oauth2/default'} client_id={'0oai9gwg86y6bzzft0h7'} redirect_uri={'https://sandhurst-cms.herokuapp.com/implicit/callback'}>
+          <Security issuer={config.issuer} client_id={config.client_id} redirect_uri={config.redirect_uri}>
             {/* <SecureRoute path='/' exact={true} component={OverviewTable} /> */}
             <Route path='/implicit/callback' component={ImplicitCallback} />
             <SecureRoute path='/' exact={true} component={DashNavbar} />
